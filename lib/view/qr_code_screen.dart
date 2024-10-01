c8fb43e05f477ff1a91766ebfbfb96a8e67bb5da
Jabbar Kakar <82799184+JabbarKakar@users.noreply.github.com>
2024-10-01 12:16:20 +0500

    BY JAbbar, First commit

---
+++ lib/view/qr_code_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+class QrCodeScreen extends StatefulWidget {
+  const QrCodeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<QrCodeScreen> createState() => _QrCodeScreenState();
+}
+
+class _QrCodeScreenState extends State<QrCodeScreen> {
+  String data = "";
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black45,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                'Qr Code Generator',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 35,
+                    color: Colors.white),
+              ),
+              const SizedBox(height: 20),
+              Center(
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  padding: const EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.white,
+                  ),
+                  child: data.isNotEmpty
+                      ? QrImageView(
+                      data: data, version: QrVersions.auto, size: 300)
+                      : Center(
+                      child: Text("Your QR code here....",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                              color: Colors.black)
+                      )
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                cursorColor: Colors.black,
+                onChanged: (value) {
+                  setState(() {
+                    data = value;
+                  });
+                },
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.black,
+                ),
+                decoration: InputDecoration(
+                  hintText: "Type here...",
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: const BorderSide(color: Colors.transparent),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+            ],
+          )
+          ),
+        ),
+      ),
+    );
+  }
+}
